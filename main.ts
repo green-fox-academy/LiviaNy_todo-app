04ea4f3cd7dcfe47c455d030597f790b0e37aa18
LiviaNy <livia.nyitrai@gmail.com>
2021-01-29 20:27:46 +0100

    switches

--- main.ts
+++ main.ts
@@ -4,4 +4,12 @@ console.log(process.argv);
 
 if (process.argv[2] === `-l`) {
   console.log(tasks);
+} else if (process.argv[2] === `-a`) {
+  //add new task
+} else if (process.argv[2] === `-r`) {
+  //removes a task
+} else if (process.argv[2] === `-c`) {
+  //complete a task
+} else {
+  console.log(`No such command`);
 }
