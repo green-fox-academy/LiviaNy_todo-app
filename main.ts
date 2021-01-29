d198142ce5370573562b6625e70b3f92b1cf143e
LiviaNy <livia.nyitrai@gmail.com>
2021-01-29 21:42:22 +0100

    -l switch prints tasks

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 `use strict`;
+const fs = require(`fs`);
+
 let tasks: string[] = [];
-console.log(process.argv);
+// console.log(process.argv);
 if (process.argv.length === 2) {
   console.log(
     `Command Line Todo applivation \n============================\nCommand line arguments:\n    -l  List all tasks\n    -a  Adds a new task\n    -r  Remove a task\n    -c  Complete a task`
   );
 } else {
   if (process.argv[2] === `-l`) {
-    console.log(tasks);
+    let fileContent = fs.readFileSync(`./tasks.txt`, `utf-8`);
+    console.log(fileContent);
   } else if (process.argv[2] === `-a`) {
     //add new task
   } else if (process.argv[2] === `-r`) {
